7becd504e9206e3722dc2475d7b103a897676374
ashwinth2312 <ashwinthkumar003@gmail.com>
2026-02-14 12:04:16 +0530

    Add .gitignore

---
+++ server/src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+const profileRoutes = require("./routes/profile");
+app.use("/api/profile", profileRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB error:", err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
